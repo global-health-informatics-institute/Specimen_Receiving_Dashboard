3a8410edf9355387c0d20bfe1a51bd95d558e992
Timothy Mtonga <timmtonga@gmail.com>
2023-04-06 08:59:13 +0200

    Added prototype for one department dashboard

--- server/get_barcode.py
+++ server/get_barcode.py
@@ -1,17 +1,23 @@
-from flask import Flask,request
+from flask import Flask, request, render_template
 
-#define app
+# define app
 app = Flask(__name__)
 
-@app.route("/", methods = ["POST"])
-#handle Post request
-def GetBarcode():
-	data = request.get_data("ID", as_text=True)
-	with open ("File.txt", "a") as my_db:#add barcode into text file
-		my_db.write(data)
-		my_db.write("\n")
-	return "ok"
 
-if __name__=='__main__':
-	app.run(debug=True, host= "0.0.0.0")
-	
+@app.route("/", methods=["POST"])
+# handle Post request
+def GetBarcode():
+    data = request.get_data("ID", as_text=True)
+    with open("File.txt", "a") as my_db:  # add barcode into text file
+        my_db.write(data)
+        my_db.write("\n")
+    return "ok"
+
+
+@app.route("/hema_dashboard")
+def HeamatologyDashboard():
+    return render_template("HeamatologyDashboard.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
