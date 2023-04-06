40e50ee6b8571dc1e32171f4fc7472cddb01155c
Timothy Mtonga <timmtonga@gmail.com>
2023-04-06 13:32:49 +0200

    Added code to simulate recieving specimen and advancing to the next stage

---
+++ server/schema.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS specimens;
+
+CREATE TABLE specimens (
+	specimen_id INTEGER PRIMARY KEY AUTOINCREMENT,
+	accession_number TEXT NOT NULL,
+	status TEXT NOT NULL,
+	department TEXT NOT NULL,
+	time_registered TIMESTAMP NOT NULL,
+	time_received TIMESTAMP
+);
