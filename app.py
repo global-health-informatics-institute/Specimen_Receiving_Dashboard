271bda1e01a1ce63c0aa607387493e75f040c734
89Tain <manda.titani@gmail.com>
2024-05-24 10:28:02 +0200

    May24
    UI Pimping should only be used on chromium OS

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+@app.route("/dashboard/")
+@app.route("/home/")
+@app.route("/")
+def index():
+    return render_template("dashboard.base.html")
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
