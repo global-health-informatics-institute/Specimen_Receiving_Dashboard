5b5c41f4618d3f1eb7e0cc513f2ac4ccfce6c511
89Tain <manda.titani@gmail.com>
2024-05-28 12:04:20 +0200

    May28
    IMplementation of model functionality

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from models.dbSetup import dbSetup
-from models.dbModel import Summary
+from models.summaryData import SummaryData
 
 app = Flask(__name__)
 
@@ -11,7 +11,12 @@ def index():
 
 if __name__ == "__main__":
     dbSetup()
-    registered = Summary('tests', 'testType', 'registered')
-    totalRestered = registered.getSummaryTotal()
-    print(f"Total is: {totalRestered}")
+
+
+    # registered = Summary('tests', 'testType', 'registered')
+    # totalRegistered = registered.getSummaryTotal()
+    summaryDataObj = SummaryData()
+    totalRegistered = summaryDataObj.getSummaryRegistered()
+    summaryDataObj.closeConn()
+    print(f"Total registered: {totalRegistered}")
     app.run(debug=True)
---
+++ models/intermediate.py
@@ -0,0 +1,24 @@
+# ToDo: implement the following: fetch data from IBlisDB within 3 days, move it to intermediate migrateAll()
+# ToDo: update summaryData()
+# ToDo: update tests()
+# ToDo: update weekly_summary()
+# ToDo: update monthly_summary()
+
+def migrateData():
+    return ""
+    # connect to iBlis database
+    # fetch all data from virtual db test (virtual coss we joining couple of tables and selective columns)
+    # filter in only entries within 3 days from date.now
+    # return assosiative array from every record
+    # update sqlite table "tests" in intermediate.db
+
+
+    
+# ll run every 5 mins triggered by a cron job and update the fields the intermediate
+def updateSummaryFields(self):
+    return ""
+    # get number of rows wit status.register from table test and update to summary
+    # get number of rows wit status.register from table test and update to summary
+    # get number of rows wit status.register from table test and update to summary
+    # get number of rows wit status.register from table test and update to summary
+    # get number of rows wit status.register from table test and update to summary
