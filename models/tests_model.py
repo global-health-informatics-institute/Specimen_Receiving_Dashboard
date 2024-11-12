93efd1535cefbfdbcedd4a3c4d146309ed1c6338
89Tain <manda.titani@gmail.com>
2024-11-12 12:02:56 +0200

    initial commit with  factory pattern, extensions definition and models, and example configurations

---
+++ models/tests_model.py
@@ -0,0 +1,17 @@
+from extensions.extensions import db, dt
+
+
+class Test(db.Model):
+    """"instance of every running test"""
+    __tablename__ = 'tests'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    tests_accession_id = db.Column(db.String(50), nullable=True, index=True, info={'Description':'in the table specimens, they reference tests with assession ids'})
+    tests_test_type = db.Column(db.String(10), nullable = True,  index = True, info={'Description':'With the reference of test_definitions, the numeric representation of ongoinga test'})
+    tests_test_status = db.Column(db.String(100), nullable=True, index=True, info={'Description':'Single digits to represent the status for each test for the dashboard'})
+    test_status = db.Column(db.Integer, nullable = False, index=True, default=1, info={'Description':'This can be used to easily trash data'})
+    created_at = db.Column(db.TIMESTAMP, nullable=False, default=dt.now, index=True)
+    updated_at = db.Column(db.TIMESTAMP, nullable=False, default=dt.now, onupdate=dt.now, index=True, info={'Description': 'used inplace of write_time'})
+    
+    def __repr__(self) -> str:
+        return f'<test (id={self.id}) - (accession_id={self.tests_accession_id})>'
---
+++ models/weekly_count_model.py
@@ -0,0 +1,20 @@
+from extensions.extensions import db, dt
+
+
+class Weekly_Count(db.Model):
+    """"Since first day of the week count"""
+    __tablename__ = 'weekly_count'
+    
+    id = db.Column(db.Integer, primary_key=True, nullable=False, default=1, index=True)
+    weekly_count_registered = db.Column(db.Integer, default=0, index=True)
+    weekly_count_received = db.Column(db.Integer, default=0, index=True)
+    weekly_count_progress = db.Column(db.Integer, default=0, index=True)
+    weekly_count_pending = db.Column(db.Integer, default=0, index=True)
+    weekly_count_complete = db.Column(db.Integer, default=0, index=True)
+    weekly_count_rejected = db.Column(db.Integer, default=0, index=True)
+    Weekly_Count_status = db.Column(db.Integer, nullable = False, index=True, default=1, info={'Description':'This can be used to easily trash data'})
+    created_at = db.Column(db.TIMESTAMP, nullable=False, default=dt.now, index=True)
+    updated_at = db.Column(db.TIMESTAMP, nullable=False, default=dt.now, onupdate=dt.now, index=True)
+    
+    def __repr__(self) -> str:
+        return f'<weekly_count (id={self.id}) - (active_status={self.Weekly_Count_status})>'
