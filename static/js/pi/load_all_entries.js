7a9059f2c351d3e9b3bb84ee88dbc9acd4a7319e
89Tain <manda.titani@gmail.com>
2025-08-27 15:40:07 +0200

    refactor: update AJAX success handlers and variable names for clarity

--- static/js/pi/load_all_entries.js
+++ static/js/pi/load_all_entries.js
@@ -20,157 +20,169 @@ $(document).ready(function() {
 
 
 
-// $(document).ready(function() {
-//     function fetchTestContent1() {
-//         $.ajax({
-//             url: "/test_data_1",
-//             method: "GET",
-//             success: function(data) {
-//                 console.log("Fetched:", data);
-//                 $("#received1").text(data.received1);
-//                 $("#inProgress1").text(data.inProgress1);
-//                 $("#pendingAuth1").text(data.pendingAuth1);
-//                 $("#complete1").text(data.complete1);
-//             }
-//         });
-//     }
+$(document).ready(function() {
+    function fetchTestContent1() {
+        $.ajax({
+            url: "/test_data_1",
+            method: "GET",
+            success: function(data) {
+                console.log("Fetched:", data);
+                $("#received1").text(data.received_1);
+                $("#inProgress1").text(data.in_progress_1);
+                $("#pendingAuth1").text(data.pending_auth_1);
+                $("#complete1").text(data.completed_1);
+            }
+        });
+    }
 
-//     fetchTestContent1();
-//     setInterval(fetchTestContent1, 1300); 
-// });
-
-
-// $(document).ready(function() {
-//     function fetchTestContent2() {
-//         $.ajax({
-//             url: "/test_data_2",
-//             method: "GET",
-//             success: function(data) {
-//                 $("#received2").text(data.received2);
-//                 $("#inProgress2").text(data.inProgress2);
-//                 $("#pendingAuth2").text(data.pendingAuth2);
-//                 $("#complete2").text(data.complete2);
-//             }
-//         });
-//     }
-//     fetchTestContent2();
-//     setInterval(fetchTestContent2, 1300); 
-// });
+    fetchTestContent1();
+    setInterval(fetchTestContent1, 1300); 
+});
 
 
-// $(document).ready(function() {
-//     function fetchTestContent3() {
-//         $.ajax({
-//             url: "/test_data_3",
-//             method: "GET",
-//             success: function(data) {
-//                 $("#received3").text(data.received3);
-//                 $("#inProgress3").text(data.inProgress3);
-//                 $("#pendingAuth3").text(data.pendingAuth3);
-//                 $("#complete3").text(data.complete3);
-//             }
-//         });
-//     }
-
-//     fetchTestContent3();
-//     setInterval(fetchTestContent3, 1300); 
-// });
-
-
-// $(document).ready(function() {
-//     function fetchTestContent4() {
-//         $.ajax({
-//             url: "/test_data_4",
-//             method: "GET",
-//             success: function(data) {
-//                 $("#received4").text(data.received4);
-//                 $("#inProgress4").text(data.inProgress4);
-//                 $("#pendingAuth4").text(data.pendingAuth4);
-//                 $("#complete4").text(data.complete4);
-//             }
-//         });
-//     }
-//     fetchTestContent4();
-//     setInterval(fetchTestContent4, 1300); 
-// });
+$(document).ready(function() {
+    function fetchTestContent2() {
+        $.ajax({
+            url: "/test_data_2",
+            method: "GET",
+            success: function(data) {
+                $("#received2").text(data.received_2);
+                $("#inProgress2").text(data.in_progress_2);
+                $("#pendingAuth2").text(data.pending_auth_2);
+                $("#complete2").text(data.completed_2);
+            }
+        });
+    }
+    fetchTestContent2();
+    setInterval(fetchTestContent2, 1300); 
+});
 
 
+$(document).ready(function() {
+    function fetchTestContent3() {
+        $.ajax({
+            url: "/test_data_3",
+            method: "GET",
+            success: function(data) {
+                $("#received3").text(data.received_3);
+                $("#inProgress3").text(data.in_progress_3);
+                $("#pendingAuth3").text(data.pending_auth_3);
+                $("#complete3").text(data.completed_3);
+            }
+        });
+    }
+
+    fetchTestContent3();
+    setInterval(fetchTestContent3, 1300); 
+});
 
 
-// $(document).ready(function() {
-//     function fetchWeeklyContent() {
-//         $.ajax({
-//             url: "/weekly_content",
-//             method: "GET",
-//             success: function(data) {
-//                 $("#weeklyRegistered").text(data.weeklyRegistered);
-//                 $("#weeklyRecieved").text(data.weeklyRecieved);
-//                 $("#weeklyProgress").text(data.weeklyProgress);
-//                 $("#weeklyComplete").text(data.weeklyComplete);
-//                 $("#weeklyRejected").text(data.weeklyRejected);
-//             }
-//         });
-//     }
-//     fetchWeeklyContent();
-//     setInterval(fetchWeeklyContent, 1300);
-// });
-
-// $(document).ready(function() {
-//     function fetchMonthlyContent() {
-//         $.ajax({
-//             url: "/monthly_content",
-//             method: "GET",
-//             success: function(data) {
-//                 $("#monthlyRegistered").text(data.monthlyRegistered);
-//                 $("#monthlyRecieved").text(data.monthlyRecieved);
-//                 $("#monthlyProgress").text(data.monthlyProgress);
-//                 $("#monthlyComplete").text(data.monthlyComplete);
-//                 $("#monthlyRejected").text(data.monthlyRejected);
-//             }
-//         });
-//     }
-
-//     fetchMonthlyContent();
-//     setInterval(fetchMonthlyContent, 1300); 
-// });
+$(document).ready(function() {
+    function fetchTestContent4() {
+        $.ajax({
+            url: "/test_data_4",
+            method: "GET",
+            success: function(data) {
+                $("#received4").text(data.received_4);
+                $("#inProgress4").text(data.in_progress_4);
+                $("#pendingAuth4").text(data.pending_auth_4);
+                $("#complete4").text(data.completed_4);
+            }
+        });
+    }
+    fetchTestContent4();
+    setInterval(fetchTestContent4, 1300); 
+});
 
 
 
-// $(document).ready(function() {
-//     function fetchTatCurrent() {
-//         $.ajax({
-//             url: "/tat_current",
-//             method: "GET",
-//             success: function(data) {
-//                 $("#current1").text(data.current1);
-//                 $("#current2").text(data.current2);
-//                 $("#current3").text(data.current3);
-//                 $("#current4").text(data.current4);
-//             }
-//         });
-//     }
 
-//     fetchTatCurrent();
-//     setInterval(fetchTatCurrent, 1300); 
-// });
+// weekly
+$(document).ready(function() {
+    function fetchWeeklySummaryContent() {
+        $.ajax({
+            url: "/weekly_summary_data",
+            method: "GET",
+            success: function(data) {
+                $("#weeklyRegistered").text(data.weekly_count_registered);
+                $("#weeklyRecieved").text(data.weekly_count_received);
+                
+                $("#weeklyProgress").text(data.weekly_count_progress);
+
+                $("#weeklyPending").text(data.weekly_count_pending);
+
+                $("#weeklyComplete").text(data.weekly_count_complete);
+
+                $("#weeklyRejected").text(data.weekly_count_rejected);
+            }
+        });
+    }
+
+    fetchWeeklySummaryContent();
+    setInterval(fetchWeeklySummaryContent, 1300); 
+});
+
+// monthly
+$(document).ready(function() {
+    function fetchMonthlySummaryContent() {
+        $.ajax({
+            url: "/monthly_summary_data",
+            method: "GET",
+            success: function(data) {
+                $("#monthlyRegistered").text(data.monthly_count_registered);
+                $("#monthlyRecieved").text(data.monthly_count_received);
+                
+                $("#monthlyProgress").text(data.monthly_count_progress);
+
+                $("#monthlyPending").text(data.monthly_count_pending);
+
+                $("#monthlyComplete").text(data.monthly_count_complete);
+
+                $("#monthlyRejected").text(data.monthly_count_rejected);
+            }
+        });
+    }
+
+    fetchMonthlySummaryContent();
+    setInterval(fetchMonthlySummaryContent, 1300); 
+});
 
 
-// $(document).ready(function() {
-//     function fetchTatAverage() {
-//         $.ajax({
-//             url: "/tat_average",
-//             method: "GET",
-//             success: function(data) {
-//                 $("#average1").text(data.average1);
-//                 $("#average2").text(data.average2);
-//                 $("#average3").text(data.average3);
-//                 $("#average4").text(data.average4);
-//             }
-//         });
-//     }
+$(document).ready(function() {
+    function fetchTatCurrent() {
+        $.ajax({
+            url: "/tat_current",
+            method: "GET",
+            success: function(data) {
+                $("#current1").text(data.current_1);
+                $("#current2").text(data.current_2);
+                $("#current3").text(data.current_3);
+                $("#current4").text(data.current_4);
+            }
+        });
+    }
 
-//     fetchTatAverage();
-//     setInterval(fetchTatAverage, 1300); 
-// });
+    fetchTatCurrent();
+    setInterval(fetchTatCurrent, 1300); 
+});
+
+
+$(document).ready(function() {
+    function fetchTatAverage() {
+        $.ajax({
+            url: "/tat_average",
+            method: "GET",
+            success: function(data) {
+                $("#average1").text(data.average_1);
+                $("#average2").text(data.average_2);
+                $("#average3").text(data.average_3);
+                $("#average4").text(data.average_4);
+            }
+        });
+    }
+
+    fetchTatAverage();
+    setInterval(fetchTatAverage, 1300); 
+});
 
 
